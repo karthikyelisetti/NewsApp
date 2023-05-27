bc771d7b4961503fa667e7864cc3da0706af041a
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-05-27 16:49:27 +0530

    WIP: updated bogdetails js file

--- newsapplication/src/components/Blogdetails.js
+++ newsapplication/src/components/Blogdetails.js
@@ -6,13 +6,13 @@ import century from "./gillcentury.webp";
 export default function Blogdetails() {
   return (
     <div>
-      <main class="container">
-        <div class="col d-flex p-4 p-md-5 mb-4 rounded text-bg-dark">
-          <div class="col-md-6 px-0">
-            <h3 class="display-4 fst-italic">
+      <main className="container">
+        <div className="col d-flex p-4 p-md-5 mb-4 rounded text-bg-dark">
+          <div className="col-md-6 px-0">
+            <h3 className="display-4 fst-italic">
               Subhman Gill Scores Third IPL Century of Season
             </h3>
-            <p class="lead my-3">
+            <p className="lead my-3">
               Shubman Gill scored his third IPL century in the match between the
               Gujrat Titans and Mumbai Indians. Gill played a knock of 129 in 60
               balls to help GT put up a massive score of 200. Gill is the third
@@ -25,24 +25,24 @@ export default function Blogdetails() {
           </div>
         </div>
 
-        <div class="row mb-2">
-          <div class="col-md-6">
-            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-              <div class="col p-4 d-flex flex-column position-static">
-                <strong class="d-inline-block mb-2 text-primary">Hockey</strong>
-                <h5 class="mb-0">
+        <div className="row mb-2">
+          <div className="col-md-6">
+            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+              <div className="col p-4 d-flex flex-column position-static">
+                <strong className="d-inline-block mb-2 text-primary">Hockey</strong>
+                <h5 className="mb-0">
                   FIH Pro League Hockey | India concede late goal to lose 1-2 to
                   Belgium
                 </h5>
-                <div class="mb-1 text-body-secondary">Mar 26, 2023</div>
-                <p class="card-text mb-auto">
+                <div className="mb-1 text-body-secondary">Mar 26, 2023</div>
+                <p className="card-text mb-auto">
                   All the goals in the match came through penalty corners.
                 </p>
-                <a href="#" class="stretched-link">
+                <a href="#" className="stretched-link">
                   Continue reading
                 </a>
               </div>
-              <div class="col-auto d-none d-lg-block">
+              <div className="col-auto d-none d-lg-block">
                 <img
                   src={hockeyImage}
                   alt="Hockey"
@@ -51,25 +51,25 @@ export default function Blogdetails() {
               </div>
             </div>
           </div>
-          <div class="col-md-6">
-            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-              <div class="col p-4 d-flex flex-column position-static">
-                <strong class="d-inline-block mb-2 text-success">
+          <div className="col-md-6">
+            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+              <div className="col p-4 d-flex flex-column position-static">
+                <strong className="d-inline-block mb-2 text-success">
                   Political
                 </strong>
-                <h5 class="mb-0">
+                <h5 className="mb-0">
                   Bhutan-China boundary expert group talks pick up speed,
                   officials hold second meeting this year
                 </h5>
-                <div class="mb-1 text-body-secondary">Nov 11</div>
-                <p class="mb-auto">
+                <div className="mb-1 text-body-secondary">Nov 11</div>
+                <p className="mb-auto">
                   EGM discusses progress in '3-step roadmap'.
                 </p>
-                <a href="#" class="stretched-link">
+                <a href="#" className="stretched-link">
                   Continue reading
                 </a>
               </div>
-              <div class="col-auto d-none d-lg-block">
+              <div className="col-auto d-none d-lg-block">
                 <img
                   src={indiachina}
                   alt="India China talks"
